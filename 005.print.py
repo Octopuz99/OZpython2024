6ff011e3aca9ff6d44a0a3b7090cba1586770813
Octopuz99 <octavio.zenteno1@hotmail.com>
2024-01-18 21:05:38 -0600

    Cargar practica de hoy Enero 18 2024

--- 005.print.py
+++ 005.print.py
@@ -1,7 +1,7 @@
 '''
-005 Print
+005 print
 es una funcion que imprime en terminal
-print (objeto=variable/constante; sep=Separador; end=final; file =recibe archivo; flush)
+print objeto=variable/constante; sep=Separador; end=final; file =recibe archivo; flush
 '''
 
 course_name ='Python 2024'
@@ -11,6 +11,5 @@ definition = 'Learning Python'
 #print(course_name,definition, sep='-----')
 #print( course_name, definition, end='*****')
 
-'''
 print(course_name, end= ' ')
 print(definition, end= ' ')
